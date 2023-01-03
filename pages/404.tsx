d8a96d7399d916dfe1b3ba1793d45b0b257f008e
Andrew Kozlitskiy <andrey.kozlitskiy@gmail.com>
2023-01-03 16:16:13 +0100

    Add 404

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import MainContainer from '../components/MainContainer/MainContainer'
+import styles from '../styles/Home.module.css'
+
+export default function Routing() {
+
+  return (
+	<MainContainer>
+		<div style={{fontSize: '50px', padding: '50px'}}>
+			Custom 404
+		</div>
+	</MainContainer>
+  )
+}
