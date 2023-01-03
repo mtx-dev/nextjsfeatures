09fa9e953276a71001e34017cd9e3fecd75fea79
Andrew Kozlitskiy <andrey.kozlitskiy@gmail.com>
2023-01-03 19:23:20 +0100

    Add post type

--- types.ts
+++ types.ts
@@ -11,3 +11,11 @@ export type addressType = {
 	email: string,
 	address: addressType,
   };
+
+  export type postType = {
+	id: number;
+	iserId: number;
+	title: string;
+	body: string;
+	createdAt?: string;
+  }
