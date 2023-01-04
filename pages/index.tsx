5ca49161003c45608fd07f0b3667980dc00c5e6c
Andrew Kozlitskiy <andrey.kozlitskiy@gmail.com>
2023-01-04 23:29:35 +0100

    remove useless

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import MainContainer from '../components/MainContainer/MainContainer'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
